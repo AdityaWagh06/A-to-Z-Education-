af1014037919d97e4c6dfc9ccfdbc2bd2b78d061
AdityaWagh06 <adityawagh2525@gmail.com>
2026-04-14 17:01:37 +0530

    Update CORS allowlist for production custom domain

--- server/index.js
+++ server/index.js
@@ -57,7 +57,6 @@ const allowedOrigins = [
     ...(process.env.CLIENT_ORIGINS || '').split(',').map((origin) => origin.trim()).filter(Boolean),
     'http://localhost:5173',
     'http://127.0.0.1:5173',
-    'https://a-to-z-education-d98u.vercel.app',
     'https://atozeducation.study'
 ].filter(Boolean);
 
